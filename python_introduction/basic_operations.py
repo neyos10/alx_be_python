01433fd38a081779380bfc3de2a8e391714cf8f5
Younes ElJaoual <eljaoual123@gmail.com>
2024-06-29 22:48:53 +0100

    tt

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+add = number1 + number2
+sub = number1 - number2
+mul = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {add}")
+print(f"Subtraction of {number1} and {number2} is {sub}")
+print(f"Multiplication of {number1} and {number2} is {mul}")
